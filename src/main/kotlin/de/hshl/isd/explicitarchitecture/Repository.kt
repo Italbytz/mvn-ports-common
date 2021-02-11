762fd1176048d903033dfd452b488356598714a0
Robin Nunkesser <robin.nunkesser@hshl.de>
2021-02-11 13:32:20 +0100

    Version 0.1.0

--- src/main/kotlin/de/hshl/isd/explicitarchitecture/Repository.kt
+++ src/main/kotlin/de/hshl/isd/explicitarchitecture/Repository.kt
@@ -2,6 +2,10 @@ package de.hshl.isd.explicitarchitecture
 
 /**
  * A `Repository` represents a data storage.
+ *
+ * In accordance with the official error handling suggestions, it is assumed that failures
+ * in the repository do not require local handling by the caller, and should therefore be represented by exceptions.
+ * @see <a href="https://github.com/Kotlin/KEEP/blob/master/proposals/stdlib/result.md#error-handling-style-and-exceptions">Error handling style</a>
  */
 interface Repository<IdType, EntityType> {
 
@@ -11,7 +15,7 @@ interface Repository<IdType, EntityType> {
      * @param entity The entity to create.
      * @return ID of the created entity.
      */
-    suspend fun create(entity: EntityType): Result<Long>
+    suspend fun create(entity: EntityType): IdType
 
     /**
      * Retrieves an entity.
@@ -19,14 +23,14 @@ interface Repository<IdType, EntityType> {
      * @param id ID of the entity to retrieve.
      * @return An entity.
      */
-    suspend fun retrieve(id: IdType): Result<EntityType>
+    suspend fun retrieve(id: IdType): EntityType
 
     /**
      * Retrieves all entities.
      *
      * @return All entities.
      */
-    suspend fun retrieveAll(): Result<List<EntityType>>
+    suspend fun retrieveAll(): List<EntityType>
 
     /**
      * Updates an entity.
@@ -35,7 +39,7 @@ interface Repository<IdType, EntityType> {
      * @param entity Updated entity.
      * @return Boolean indicating success or failure.
      */
-    suspend fun update(id: IdType, entity: EntityType): Result<Boolean>
+    suspend fun update(id: IdType, entity: EntityType): Boolean
 
     /**
      * Deletes an entity.
@@ -43,5 +47,6 @@ interface Repository<IdType, EntityType> {
      * @param id ID of the entity to delete.
      * @return Boolean indicating success or failure.
      */
-    suspend fun delete(id: IdType): Result<Boolean>
+    suspend fun delete(id: IdType): Boolean
+
 }
